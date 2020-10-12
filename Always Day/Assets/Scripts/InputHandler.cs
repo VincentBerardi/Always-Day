69cd19eb4f3ebc7edbe875e5d063940f50a78085
OmarAlFarajat <40572852+OmarAlFarajat@users.noreply.github.com>
2020-10-12 18:29:55 -0400

    AudioManager and InputHandler

---
+++ Always Day/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    // For testing
+    private AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();    
+    }
+
+    void Update()
+    {
+        CheckInputs();
+    }
+
+    void CheckInputs()
+    {
+        // For testing
+        if (Input.GetKeyDown(KeyCode.Alpha1) && audioManager != null)
+            audioManager.Play("test");
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && audioManager != null)
+            audioManager.Stop("test");
+    }
+}
