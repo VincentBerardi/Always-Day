b7616e13d746f8786b760005d0ac0ba28cbc21f9
VincentBerardi <vincent.l.berardi@gmail.com>
2020-10-13 19:02:13 -0400

    Created circle images and started logic for light absorb bar and stun bar scripts

---
+++ Always Day/Assets/Scripts/StunBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StunBar : MonoBehaviour
+{
+    public Image stunBarImg;
+
+    public float StunBarValue { get; set; } = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StunBarProgress(5.0f * Time.deltaTime);
+    }
+
+    public void StunBarProgress(float stunBarFillValue)
+    {
+        float fillAmount = stunBarFillValue / 100.0f;
+        stunBarImg.fillAmount += fillAmount;
+    }
+}
