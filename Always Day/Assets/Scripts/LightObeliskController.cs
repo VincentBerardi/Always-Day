54a66e06d8c3ab0e183ffc452eff4e98109319f1
VincentBerardi <vincent.l.berardi@gmail.com>
2020-10-14 10:33:03 -0400

    Finished light absorption logic in terms of diminishing light emission when player is near -created an emissive green light material -added/installed Unity's PPE package -added script with logic for absorption of light when player is near

---
+++ Always Day/Assets/Scripts/LightObeliskController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightObeliskController : MonoBehaviour
+{
+    public float nearRadius;
+    Vector3 lightObeliskPos;
+    public float emissionIntensity;
+    public float emissionLossRate;
+    public GameObject player;
+    Vector3 playerPos;
+
+    public Renderer renderer;
+
+    public Material mat;
+    Color color;
+
+    public LightAbsorbBar lightAbsorbBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nearRadius = 0.8f;
+        emissionLossRate = 0.1f;
+        lightObeliskPos = transform.position;
+        playerPos = player.transform.position;
+        color = mat.color;
+        emissionIntensity = 3.416924f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = player.transform.position;
+
+        if(Math.Abs(lightObeliskPos.magnitude - playerPos.magnitude) <= nearRadius )
+            PlayerIsNearEnoughToAbsorbLight();
+    }
+
+    private void PlayerIsNearEnoughToAbsorbLight()
+    {
+        float amountOfLightAbsorbed = 5.0f * Time.deltaTime;
+        lightAbsorbBar.enabled = true;
+        lightAbsorbBar.LightAbsorbBarProgress(amountOfLightAbsorbed);
+        
+        Debug.Log(emissionIntensity);
+        if (emissionIntensity > 0.8f)
+        {
+            emissionIntensity -= Time.deltaTime * emissionLossRate;
+            renderer.material.SetColor("_EmissionColor", color * emissionIntensity);
+            renderer.UpdateGIMaterials();
+        }
+    }
+}
