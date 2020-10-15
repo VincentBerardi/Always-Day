f1ef27fd7165b863c6ceaa0d712fd5d039327e4f
Lyonel Zamora <lyonelz516@gmail.com>
2020-10-15 01:49:39 -0400

    Add a simple StateMachine example

---
+++ Always Day/Assets/Scripts/StateMachine/Example/MovingStateMachine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingStateMachine : MonoBehaviour
+{
+    StateMachine stateMachine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stateMachine = new StateMachine();
+        stateMachine.CurrentState = new MoveForwardState(stateMachine, this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.CurrentState.Update();
+    }
+}
