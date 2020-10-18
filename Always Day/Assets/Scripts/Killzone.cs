0a51ba2085ae33fce3b7371fdc402e2361b99850
OmarAlFarajat <40572852+OmarAlFarajat@users.noreply.github.com>
2020-10-18 02:46:22 -0400

    Added out-of-bounds killzones and respawn points.

---
+++ Always Day/Assets/Scripts/Killzone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killzone : MonoBehaviour
+{
+    public GameObject respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player")) {
+            other.transform.GetComponent<PlayerController>().Die(respawnPoint.transform.position);
+        }
+    }
+}
