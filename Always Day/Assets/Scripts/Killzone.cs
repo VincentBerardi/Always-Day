5b58890176ffd6dbaa517c2640c8729191a8401e
OmarAlFarajat <40572852+OmarAlFarajat@users.noreply.github.com>
2020-10-18 13:02:53 -0400

    Hazards Logic and Placement

--- Always Day/Assets/Scripts/Killzone.cs
+++ Always Day/Assets/Scripts/Killzone.cs
@@ -9,6 +9,7 @@ public class Killzone : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Player")) {
+            if(other.transform.GetComponent<PlayerController>().isDead != true)
             other.transform.GetComponent<PlayerController>().Die(respawnPoint.transform.position);
         }
     }
